3eac18bac657b2f71a584b233e1d02e2a669b73c
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-08-18 09:32:18 -0300

    feat: Criando um Link - model, migration, factory, form requests, controller e views.

---
+++ resources/views/links/create.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <h1>Criar um Link</h1>
+
+    <div>
+        @if($message = session()->get('message'))
+            <div>
+                {{ $message }}
+            </div>
+        @endif
+        <form action="{{ route('links.create') }}" method="post">
+            @csrf
+             <div>
+                <input type="link" name="link" placeholder="Link" />
+                @error('link')
+                <span>{{ $message }}</span>
+                @enderror
+            </div>
+            <br>
+            <div>
+                <input name="name" placeholder="Nome" />
+                @error('name')
+                <span>{{ $message }}</span>
+                @enderror
+            </div>
+            <br>
+            <button>Salvar</button>
+        </form>
+    </div>
+</div>
