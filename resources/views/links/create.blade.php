05767f7336651d8a24bb6d2ea6ddd947a4afedad
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-09-26 10:56:42 -0300

    fix: refatorando layout

--- resources/views/links/create.blade.php
+++ resources/views/links/create.blade.php
@@ -1,31 +1,16 @@
 <x-layout.app>
-<div>
-    <h1>Criar um Link</h1>
 
-    <div>
-        @if($message = session()->get('message'))
-            <div>
-                {{ $message }}
-            </div>
-        @endif
-        <form action="{{ route('links.create') }}" method="post">
-            @csrf
-             <div>
-                <input type="link" name="link" placeholder="Link" />
-                @error('link')
-                <span>{{ $message }}</span>
-                @enderror
-            </div>
-            <br>
-            <div>
-                <input name="name" placeholder="Nome" />
-                @error('name')
-                <span>{{ $message }}</span>
-                @enderror
-            </div>
-            <br>
-            <button>Salvar</button>
-        </form>
-    </div>
-</div>
+    <x-container class="h-full">
+        <x-card title="Create a new link">
+            <x-form :route="route('links.create')" post id="link-form">
+                <x-input name="link" type="link" placeholder="Link" value="{{old('link')}}"/>
+                <x-input name="name" type="name" placeholder="Nome" value="{{old('name')}}"/>
+            </x-form>
+            <x-slot:actions>
+                <x-a :href="route('dashboard')" class="link-primary link-hover">Cancel</x-a>
+                <x-button type="submit" form="link-form">Create a new link</x-button>
+            </x-slot:actions>
+        </x-card>
+
+    </x-container>
 </x-layout.app>
